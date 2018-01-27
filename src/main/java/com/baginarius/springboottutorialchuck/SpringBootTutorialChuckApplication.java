152bef86c6d3e438ec68ac6e3e9281e2c5834ad8
mbaginski <mbaginski@consdata.pl>
2018-01-27 17:59:04 +0100

    Spring boot configuration

---
+++ src/main/java/com/baginarius/springboottutorialchuck/SpringBootTutorialChuckApplication.java
@@ -0,0 +1,12 @@
+package com.baginarius.springboottutorialchuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialChuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialChuckApplication.class, args);
+	}
+}
