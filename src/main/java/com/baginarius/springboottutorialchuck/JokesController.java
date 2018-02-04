18c91fa62c1cf76d3e5738233417a1cae0cead1e
mbaginski <mbaginski@consdata.pl>
2018-02-04 15:25:01 +0100

    Refactor - getting joke via service

--- src/main/java/com/baginarius/springboottutorialchuck/JokesController.java
+++ src/main/java/com/baginarius/springboottutorialchuck/JokesController.java
@@ -1,5 +1,7 @@
 package com.baginarius.springboottutorialchuck;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,12 @@ public class JokesController {
 
     @RequestMapping("/")
     public String getJoke(Model model) {
+        System.out.println(env.getProperty("xxx"));
         model.addAttribute("joke", jokeGetter.getJoke());
         return "chucknorris";
     }
+
+    @Autowired
+    private Environment env;
 
 }
--- src/main/java/com/baginarius/springboottutorialchuck/JokesInjector.java
+++ src/main/java/com/baginarius/springboottutorialchuck/JokesInjector.java
@@ -1,11 +1,16 @@
 package com.baginarius.springboottutorialchuck;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class JokesInjector {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
